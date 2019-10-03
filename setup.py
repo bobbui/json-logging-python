de75f9b3824061b110ddc067f41d4df6ed945194
Bob - Bui Nguyen Thang <5661982+thangbn@users.noreply.github.com>
2019-10-02 18:33:19 -0700

    bump to 1.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name="json-logging",
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'example', 'dist', 'build']),
     license='Apache License 2.0',
     description="JSON Python Logging",
