f8987c94e5b2de89a56ca60328223c10df9383c1
Bob Bui <bob.bui@adperfect.com>
2021-11-15 12:34:53 -0800

    fix: fix flask sample

--- example/flask_sample_app.py
+++ example/flask_sample_app.py
@@ -7,14 +7,40 @@ import json_logging
 
 app = flask.Flask(__name__)
 json_logging.init_flask(enable_json=True)
-json_logging.init_request_instrument(
-    app, exclude_url_patterns=[r'/ready', r'/alive'])
+json_logging.init_request_instrument(app, exclude_url_patterns=[r'/exclude_from_request_instrumentation'])
 
-app.logger.setLevel(logging.getLevelName(env.get('LOGLEVEL', 'INFO').upper()))
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.removeHandler(default_handler)
+# init the logger as usual
+logger = logging.getLogger("test logger")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
-app.config["MONGO_URI"] = env.get("MONGO_DSN")
 
-client = MongoClient(app.config["MONGO_URI"])
-app.logger.debug(client.server_info())
+@app.route('/')
+def home():
+    logger.info("test log statement")
+    logger.info("test log statement with extra props", extra={'props': {"extra_property": 'extra_value'}})
+    logger.info("test log statement with custom correlation id",
+                extra={'props': {'correlation_id': 'custom_correlation_id'}})
+
+    correlation_id = json_logging.get_correlation_id()
+    return "hello world" \
+           "\ncorrelation_id                    : " + correlation_id
+
+
+@app.route('/exception')
+def exception():
+    try:
+        raise RuntimeError
+    except BaseException as e:
+        logger.error("Error occurred", exc_info=e)
+        logger.exception("Error occurred", exc_info=e)
+    return "Error occurred, check log for detail"
+
+
+@app.route('/exclude_from_request_instrumentation')
+def exclude_from_request_instrumentation():
+    return "this request wont log request instrumentation information"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(5000), use_reloader=False)
