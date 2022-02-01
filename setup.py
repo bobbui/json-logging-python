740f85416e69c01f7beb063db0c96144b257388c
Bob Bui <bob.bui@adperfect.com>
2022-02-01 02:50:02 -0800

    chore: bump to 1.5.0-rc0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name="json-logging",
-    version='1.4.1-rc0',
+    version='1.5.0-rc0',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'example', 'dist', 'build']),
     license='Apache License 2.0',
     description="JSON Python Logging",
